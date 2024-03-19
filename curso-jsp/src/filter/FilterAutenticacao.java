7daeda097ebb3721ece2fc89cddeadc95e9b5551
henri <henri@LAPTOP-87NOKUNL>
2024-03-18 22:46:45 -0300

    22.16 banco

--- curso-jsp/src/filter/FilterAutenticacao.java
+++ curso-jsp/src/filter/FilterAutenticacao.java
@@ -1,6 +1,9 @@
 package filter;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -12,50 +15,70 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-@WebFilter(urlPatterns = {"/principal/*"}) // Intercepta todas as requisições que vierem do projeto
-//Ou mapeamento
+import connection.SingleConnectionBanco;
+
+@WebFilter(urlPatterns = { "/principal/*" }) /* Interceptas todas as requisiçoes que vierem do projeto ou mapeamento */
 public class FilterAutenticacao implements Filter {
 
+	private static Connection connection;
+	
 	public FilterAutenticacao() {
 	}
 
-//Encerra os processos quando o servidor é parado
-//Mataria o processo de conexão com o banco
+	/* Encerra os processo quando o servidor é parado */
+	/* Mataria os processo de conexão com banco */
 	public void destroy() {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
-//Intercepta as requisições e as repostas ao sistema
-//Tudo que fazer no sistema passa por aqui
-//Valdidar e fazer redirecionamentos 
+	/* Intercepta as requisicoes e a as respostas no sistema */
+	/* Tudo que fizer no sistema vai fazer por aqui */
+	/* Validação de autenticao */
+	/* Dar commit e rolback de transaçoes do banco */
+	/* Validar e fazer redirecionamento de paginas */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-
+	  try {
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpSession session = req.getSession();
 
 		String usuarioLogado = (String) session.getAttribute("usuario");
-		String urlParaAutenticar = req.getServletPath();// Url que esta sendo acessada
 
-//validar se esta logado senão redireciona para tela de login
+		String urlParaAutenticar = req.getServletPath();/* Url que está sendo acessada */
 
+		/* Validar se está logado senão redireciona para a tela de login */
 		if (usuarioLogado == null
-				|| (usuarioLogado != null && usuarioLogado.isEmpty()) &&
-				!urlParaAutenticar.contains("principal/ServeletLogin")) {//Não esta logado
-			
+				&& !urlParaAutenticar.equalsIgnoreCase("/principal/ServletLogin")) {/* Não está logado */
+
 			RequestDispatcher redireciona = request.getRequestDispatcher("/index.jsp?url=" + urlParaAutenticar);
-			request.setAttribute("msg", "Por favor realize o login");
+			request.setAttribute("msg", "Por favor realize o login!");
 			redireciona.forward(request, response);
-			return; //Para a execução e redireciona para o login
-			
-		}else {
+			return; /* Para a execução e redireciona para o login */
 
-		chain.doFilter(request, response);
+		} else {
+			chain.doFilter(request, response);
 		}
-		}
-
-//Inicia os processos ou recursos quando o servidor sobe o projeto
-//iniciar a conexão com o banco 
-	public void init(FilterConfig fConfig) throws ServletException {
+		
+		connection.commit();//Tudo certo então comita as alterações no banco 
+		
+	  }catch (Exception e) {
+		  e.printStackTrace();	
+		try {
+			connection.rollback();
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}  
+	  }
 	}
 
+	/* Inicia os processo ou recursos quando o servidor sobre o projeto */
+	// inicar a conexão com o banco
+	public void init(FilterConfig fConfig) throws ServletException {
+		connection = SingleConnectionBanco.getConnection();
+		
+	}
 }
